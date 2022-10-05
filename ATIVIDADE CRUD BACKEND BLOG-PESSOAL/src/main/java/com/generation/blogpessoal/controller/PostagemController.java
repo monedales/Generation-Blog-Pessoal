d05b7ac9352dc7de66222a8aa8bec23a7e94549a
Letícia Moneda <le.moneda@outlook.com>
2022-10-04 21:44:01 -0300

    criando a relação das tabelas temas e postagens

--- ATIVIDADE CRUD BACKEND BLOG-PESSOAL/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ ATIVIDADE CRUD BACKEND BLOG-PESSOAL/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -22,15 +22,19 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
-
+import com.generation.blogpessoal.repository.TemaRepository;
 
 @RestController
 @RequestMapping("/postagens")
 @CrossOrigin(origins = "*", allowedHeaders = "*") // serve como configuração pra fazer requisições em uma estrutura de framework
-public class PostagemController {
+public class PostagemController 
+{
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll()
@@ -55,16 +59,25 @@ public class PostagemController {
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem)
 	{
-		return ResponseEntity.status(HttpStatus.CREATED)
+		if (temaRepository.existsById(postagem.getTema().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED)
 				.body(postagemRepository.save(postagem));
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 	
 	@PutMapping
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem)
 	{
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		if (postagemRepository.existsById(postagem.getId()))
+		{
+			if(temaRepository.existsById(postagem.getTema().getId()))
+				return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
@@ -80,5 +93,3 @@ public class PostagemController {
 	}
 
 }
-
-
